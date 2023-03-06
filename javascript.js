4731cfd591cbfe58bcc407155074c01aa2c5905d
Your Name <ndimant@hotmail.com>
2023-03-06 09:23:12 +0200

    creating playRound function

--- javascript.js
+++ javascript.js
@@ -1,7 +1,17 @@
 function computerPlay() {
-  const choices = ['rock', 'paper', 'scissors'];
-  const randomIndex = Math.floor(Math.random() * 3);
-  return choices[randomIndex];
+const choices = ['rock', 'paper', 'scissors'];
+const randomIndex = Math.floor(Math.random() * 3);
+return choices[randomIndex];
 }
 
-computerPlay()
+function playRound(playerSelection, computerSelection){
+playerSelection = playerSelection.toLowerCase()
+if(playerSelection===computerSelection){
+return "its a tie!"
+} else if((playerSelection==="rock" && computerSelection==="scissors")||(playerSelection==="paper" &&
+computerSelection==="rock")||(playerSelection==="scissors" && computerSelection==="paper")){
+return `You Win! ${playerSelection} beats ${computerSelection}`
+} else{
+return `You Lose! ${computerSelection} beats ${playerSelection}`
+}
+}
