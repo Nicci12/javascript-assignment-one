81304010ac954d9b18b0523889b4fe4735512720
Your Name <ndimant@hotmail.com>
2023-03-09 11:50:36 +0200

    final finishes

--- javascript.js
+++ javascript.js
@@ -20,34 +20,32 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt("Rock, paper, or scissors?");
-    const computerSelection = computerPlay();
-    const roundResult = playRound(playerSelection, computerSelection);
-
-    console.log(roundResult);
-
-    if (roundResult.includes("Win")) {
-      playerScore++;
-    } else if (roundResult.includes("Lose")) {
-      computerScore++;
+    let playerScore = 0;
+    let computerScore = 0;
+  
+    for (let i = 0; i < 5; i++) {
+      const playerSelection = prompt("Rock, paper, or scissors?");
+      const computerSelection = computerPlay();
+      const roundResult = playRound(playerSelection, computerSelection);
+  
+      console.log(roundResult);
+  
+      if (roundResult.includes("Win")) {
+        playerScore++;
+      } else if (roundResult.includes("Lose")) {
+        computerScore++;
+      }
     }
   
-
+    console.log(`Player score: ${playerScore}`);
+    console.log(`Computer score: ${computerScore}`);
+  
     if (playerScore > computerScore) {
-      console.log(
-        `You win the game!Total Score is: PlayerScore:${playerScore}- ComputerScore: ${computerScore}`
-      );
+      console.log("Player wins!");
     } else if (playerScore < computerScore) {
-      console.log(`You Lose the game!Total Score is: ${playerScore} - Computer: ${computerScore}`)
+      console.log("Computer wins!");
     } else {
-      console.log(`It's a tie!Total Score is:
-        ${playerScore}-${computerScore}`);
+      console.log("It's a tie!");
     }
   }
-}
-
 game()
